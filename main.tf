ed873d38fdfe056fc9254d4066c2047a0fb542c3
Bob J <bobjas777@hotmail.com>
2022-11-22 22:00:33 -0800

    initial commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+  cloud {
+    organization = "bj-cloud-organization"
+    hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
+
+    workspaces {
+      tags = "common"
+    }
+  }
+}
+resource "random_pet" "vpc" {}
